95e894cb81f47c43712d9dab8aa27cf770743e44
jnarvaez <jnarvaez@lean-tech.io>
2022-03-21 14:06:26 -0500

    html-loader correctly implemented

--- src/index.js
+++ src/index.js
@@ -2,5 +2,17 @@ import "./scss/main.scss";
 
 const div = document.getElementById("root");
 
-
-console.log(div);
+div.innerHTML = `
+    <div class="main">
+        <h3 class="main_subtitle">Mentorship - Session 2:</h3>
+        <h1 class="main_title">Webpack Basics 🖥</h1>
+        <ul class="main_list">
+            <li><label><input type="checkbox" checked>Create a project with npm, git and webpack</label></li>
+            <li><label><input type="checkbox" checked>Install and use html-loader</label></li>
+            <li><label><input type="checkbox" checked>Get the main HTML element, append information and change colors</label></li>
+            <li><label><input type="checkbox" checked>Create production bundle</label></li>
+            <li><label><input type="checkbox" checked>Use git and upload the project to github</label></li>
+            <li><label><input type="checkbox" checked>Implement HotReload🌶 to Webpack</label></li>
+        </ul>
+    </div>
+`;
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,39 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.html$/i,
-                loader: "html-loader"
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass"),
             },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                  // Creates `style` nodes from JS strings
-                  "style-loader",
-                  // Translates CSS into CommonJS
-                  "css-loader",
-                  // Compiles Sass to CSS
-                  "sass-loader",
-                ],
-              },
-        ]
-    },
-    mode: 'development',
-    entry: "./src/index.js",
-    plugins: [
-        new HtmlWebpackPlugin({
-          title: 'Output Management',
-        }),
-      ],
-    output: {
-        filename:"bundle.js",
-        path: path.resolve(__dirname, "dist")
-    }
-}
+          },
+        ],
+      },
+    ],
+  },
+  mode: "development",
+  entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Session 2",
+      template: "./public/index.html"
+    }),
+  ],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};

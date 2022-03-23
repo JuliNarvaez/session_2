87c017deeec229387799b585ff8dacdf2cae4b1d
jnarvaez <jnarvaez@lean-tech.io>
2022-03-23 15:44:11 -0500

    Layout fix

--- src/index.js
+++ src/index.js
@@ -6,25 +6,27 @@ import { isDivisibleByTen } from "./isDivisibleByTen.js";
 const div = document.getElementById("root");
 
 div.innerHTML = `
-    <div class="main">
-        <h3 class="main_subtitle">Mentorship - Session 2:</h3>
-        <h1 class="main_title">Webpack Basics 🖥</h1>
-        <ul class="main_list">
-            <li><label><input type="checkbox" checked>Create a project with npm, git and webpack</label></li>
-            <li><label><input type="checkbox" checked>Install and use html-loader</label></li>
-            <li><label><input type="checkbox" checked>Get the main HTML element, append information and change colors</label></li>
-            <li><label><input type="checkbox" checked>Create production bundle</label></li>
-            <li><label><input type="checkbox" checked>Use git and upload the project to github</label></li>
-            <li><label><input type="checkbox" checked>Implement HotReload🌶 to Webpack</label></li>
-        </ul>
-    </div>
-    <div class="main">
-        <h3 class="main_subtitle">Mentorship - Session 3:</h3>
-        <h1 class="main_title">JS Basics and ES6</h1>
-        <div class="main_container">
-            <p><strong>Introduce a number and check if is EVEN or ODD and if is divisible by 10.</strong></p>
-            <input type="number" placeholder="Enter a number" id="numberValue"> <span>↵</span>
-            <div id="result"></div>
+    <div class="container">
+        <div class="main">
+            <h3 class="main_subtitle">Mentorship - Session 2:</h3>
+            <h1 class="main_title">Webpack Basics 🖥</h1>
+            <ul class="main_list">
+                <li><label><input type="checkbox" checked>Create a project with npm, git and webpack</label></li>
+                <li><label><input type="checkbox" checked>Install and use html-loader</label></li>
+                <li><label><input type="checkbox" checked>Get the main HTML element, append information and change colors</label></li>
+                <li><label><input type="checkbox" checked>Create production bundle</label></li>
+                <li><label><input type="checkbox" checked>Use git and upload the project to github</label></li>
+                <li><label><input type="checkbox" checked>Implement HotReload🌶 to Webpack</label></li>
+            </ul>
+        </div>
+        <div class="main">
+            <h3 class="main_subtitle">Mentorship - Session 3:</h3>
+            <h1 class="main_title">JS Basics and ES6</h1>
+            <div class="main_container">
+                <p><strong>Introduce a number and check if is EVEN or ODD and if is divisible by 10.</strong></p>
+                <input type="number" placeholder="Enter a number" id="numberValue"> <span>↵</span>
+                <div id="result"></div>
+            </div>
         </div>
     </div>
 `;
